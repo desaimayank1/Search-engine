29c3ae34d269af7f647c0e28c0683af878d8aef4
desaimayank1 <desaimayank478@gmail.com>
2024-09-11 21:51:02 +0530

    deployment

--- idf.js
+++ idf.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path=require("path");
-let usersPath = path.join(process.cwd(), 'IDF.txt');
-const idfstr = fs.readFileSync(usersPath).toString();
-const idf = idfstr.split("\n");
+let usersPath = path.join(process.cwd(), 'IDF.txt'); //so that server know where the txt file is present 
+const idfstr = fs.readFileSync(usersPath).toString();//process.cwd() reads the file IDF.txt from the root of the repository.
+const idf = idfstr.split("\n"); 
 
 for (let i = 0; i < idf.length; i++) {
   idf[i] = Number(idf[i]);
